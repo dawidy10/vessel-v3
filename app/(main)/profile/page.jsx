f194ad22510bb543fa11220b07cdcc3645945806
dawidy10 <davidburaga10@gmail.com>
2025-07-02 16:04:12 +0300

    Added posts to profile page

--- app/(main)/profile/page.jsx
+++ app/(main)/profile/page.jsx
@@ -1,7 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import Profile from "@/components/Profile";
 import ActivityTracker from "@/components/ActivityTracker";
-
+import ThreadPost from "@/components/ThreadPost";
+import ActivityPost from "@/components/ActivityPost";
 export default async function ProfilePage() {
 	var profileData = null;
 	const supabase = await createClient();
@@ -9,7 +10,10 @@ export default async function ProfilePage() {
 	const { data: profData, profileError } = await supabase.from("profiles").select("*").eq("id", data.user.id);
 	profileData = profData[0];
 
-	const { data: posts, postsError } = await supabase.from("posts").select("*").eq("author_id", data.user.id);
+	const { data: posts, postsError } = await supabase
+		.from("posts")
+		.select("*, profiles(id,username, name, avatar), likes(user_id)")
+		.eq("author_id", data.user.id);
 
 	// console.log(posts);
 
@@ -17,8 +21,29 @@ export default async function ProfilePage() {
 		<div className="flex flex-col items-center">
 			<div className="flex flex-col items-center w-[60vw] mt-10">
 				{profileData && <Profile profileData={profileData} />}
-
 				{posts && <ActivityTracker variant="profile" activityData={posts} />}
+				<div className="mt-10">
+					{posts ? (
+						posts.map((post, index) => (
+							<div className="mt-10" key={index}>
+								{post.type == "thread" ? (
+									<ThreadPost
+										author={post.profiles}
+										content={post}
+										likes={post.likes}
+										authUserID={data.user.id}
+									/>
+								) : (
+									<>
+										<ActivityPost author={post.profiles} content={post} authUserID={data.user.id} />
+									</>
+								)}
+							</div>
+						))
+					) : (
+						<h1 className="text-2xl">No posts found.</h1>
+					)}
+				</div>
 			</div>
 		</div>
 	);
