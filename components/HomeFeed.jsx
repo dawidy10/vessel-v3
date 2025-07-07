d684438b4e361db13eaf20397f62810dc3d6bc81
dawidy10 <davidburaga10@gmail.com>
2025-07-07 12:25:30 +0300

    Show posts only from followed users on homefeed

--- components/HomeFeed.jsx
+++ components/HomeFeed.jsx
@@ -6,10 +6,24 @@ import ActivityPost from "./ActivityPost";
 export default async function HomeFeed({ authUserID }) {
 	const cookieStore = cookies();
 	const supabase = await createClient({ cookies: () => cookieStore });
+
+	const { data: followedUsers, error: followedUsersError } = await supabase
+		.from("followers")
+		.select("following_id")
+		.eq("follower_id", authUserID)
+		.eq("accepted", true);
+
+	if (followedUsersError) {
+		console.error(followedUsersError);
+	}
+
+	const followedUserIds = followedUsers.map((follow) => follow.following_id);
+	followedUserIds.push(authUserID);
+
 	const { data: posts, error } = await supabase
 		.from("posts")
 		.select("*, profiles(id,username, name, avatar), likes(user_id)")
-		// .in("user_id", followedUserIds)
+		.in("author_id", followedUserIds)
 		.order("created_at", { ascending: false });
 	// .range(0, 4);
 
